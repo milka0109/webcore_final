ebd001ea88c713a0e2c31e20d451bdfbedd0f1b5
Anastasia Dvortsova <komaedathecloud@gmail.com>
2022-10-24 04:30:07 +0300

    я все

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,41 +1,83 @@
-const callBtn = document.querySelector('nav-bar__button-call');
-const messageBtn = document.querySelector('nav-bar__button-message');
-const requestCloseBtn = document.querySelector('.feedback-request__button-close');
+const callBtn = document.querySelector('.nav-bar__button-call');
+const messageBtn = document.querySelector('.nav-bar__button-message');
 const callRequest = document.querySelector('.feedback-request--call');
 const messageRequest = document.querySelector('.feedback-request--message');
+const messageCloseBtn = messageRequest.querySelector('.feedback-request__button-close');
+const callCloseBtn = callRequest.querySelector('.feedback-request__button-close');
 const mainRequest = document.querySelector('.main');
 const sideMenuRequest = document.querySelector('.side-menu');
 
-// if (window.innerWidth < 1440) {
+const sideMessageBtn = document.querySelector('.side-footer__button-message');
+const sideCallBtn = document.querySelector('.side-footer__button-call');
 
-//     const navBarMenuBtn = document.querySelector('.nav-bar__button-menu');
-//     const closeBtn = document.querySelector('.side-header__button-close');
-//     const sideMenu = document.querySelector('.side-menu');
-//     const main = document.querySelector('.main');
-    
-//     navBarMenuBtn.addEventListener("click", function (e) {
-//         e.stopPropagation();
-//         sideMenu.classList.remove('side-menu--hidden');
-//         sideMenu.classList.add('side-menu--visible');
-//         main.classList.add('main--blurred');
-//     });
-    
-//     closeBtn.addEventListener("click", function (e) {
-//         e.stopPropagation();
-//         sideMenu.classList.remove('side-menu--visible');
-//         sideMenu.classList.add('side-menu--hidden');
-//         main.classList.remove('main--blurred');
-//     });
-    
-//     document.addEventListener("click", function (e) {
-//         const target = e.target;
-//         const its_sideMenu = target == sideMenu || sideMenu.contains(target);
-//         const menu_is_active = sideMenu.classList.contains("side-menu--visible");
-    
-//         if (!its_sideMenu && menu_is_active) {
-//             sideMenu.classList.remove('side-menu--visible');
-//             sideMenu.classList.add('side-menu--hidden');
-//             main.classList.remove('main--blurred');
-//         }
-//     });
-//     }
+sideCallBtn.addEventListener("click", function (a) {
+    a.stopPropagation();
+    callRequest.classList.remove('feedback-request--call--hidden');
+    callRequest.classList.add('feedback-request--call--visible');
+    mainRequest.classList.add('main--blurred');
+    sideMenuRequest.classList.add('side-menu--blurred');
+});
+
+sideMessageBtn.addEventListener("click", function (a) {
+    a.stopPropagation();
+    messageRequest.classList.remove('feedback-request--message--hidden');
+    messageRequest.classList.add('feedback-request--message--visible');
+    mainRequest.classList.add('main--blurred');
+    sideMenuRequest.classList.add('side-menu--blurred');
+});
+
+callBtn.addEventListener("click", function (a) {
+    a.stopPropagation();
+    callRequest.classList.remove('feedback-request--call--hidden');
+    callRequest.classList.add('feedback-request--call--visible');
+    mainRequest.classList.add('main--blurred');
+    sideMenuRequest.classList.add('side-menu--blurred');
+});
+
+messageBtn.addEventListener("click", function (a) {
+    a.stopPropagation();
+    messageRequest.classList.remove('feedback-request--message--hidden');
+    messageRequest.classList.add('feedback-request--message--visible');
+    mainRequest.classList.add('main--blurred');
+    sideMenuRequest.classList.add('side-menu--blurred');
+});
+
+messageCloseBtn.addEventListener("click", function (a) {
+    a.stopPropagation();
+    messageRequest.classList.remove('feedback-request--message--visible');
+    messageRequest.classList.add('feedback-request--message--hidden');
+    mainRequest.classList.remove('main--blurred');
+    sideMenuRequest.classList.remove('side-menu--blurred');
+});
+
+callCloseBtn.addEventListener("click", function (a) {
+    a.stopPropagation();
+    callRequest.classList.remove('feedback-request--call--visible');
+    callRequest.classList.add('feedback-request--call--hidden');
+    mainRequest.classList.remove('main--blurred');
+    sideMenuRequest.classList.remove('side-menu--blurred');
+});
+
+document.addEventListener("click", function (a) {
+    const target = a.target;
+    const its_call= target == callRequest || callRequest.contains(target);
+    const its_message= target == callRequest || callRequest.contains(target);
+    const call_is_active = callRequest.classList.contains("feedback-request--call--visible");
+    const message_is_active = messageRequest.classList.contains("feedback-request--message--visible");
+
+    if (!its_call && call_is_active) {
+        a.stopPropagation();
+        callRequest.classList.remove('feedback-request--call--visible');
+        callRequest.classList.add('feedback-request--call--hidden');
+        mainRequest.classList.remove('main--blurred');
+        sideMenuRequest.classList.remove('side-menu--blurred');
+    }
+
+    if (!its_message && message_is_active) {
+        a.stopPropagation();
+        messageRequest.classList.remove('feedback-request--message--visible');
+        messageRequest.classList.add('feedback-request--message--hidden');
+        mainRequest.classList.remove('main--blurred');
+        sideMenuRequest.classList.remove('side-menu--blurred');
+    }
+});
